ddca70bd22e3d1e9a22df475c957c742c9bcdbdf
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-26 21:19:01 -0400

    Changing the var body to use models.Post

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -4,15 +4,14 @@ import (
 	"go_rest_gin/initializer"
 	"go_rest_gin/models"
 
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CreateMessage(c *gin.Context) {
 	// Get data from the request body
-	var body struct {
-		Body  string
-		Title string
-	}
+	var body models.Post
 
 	// This will check if Content-Type == "application/json" or application/xml to determine how to parse input.
 	c.Bind(&body)
@@ -30,5 +29,22 @@ func CreateMessage(c *gin.Context) {
 	}
 	c.IndentedJSON(200, gin.H{
 		"post": post,
+	})
+}
+
+func GetMessagebyTitle(c *gin.Context) {
+
+	var posts []models.Post
+
+	title := c.Param("title")
+	log.Println("The param is", title)
+
+	result := initializer.DB.Where("title = ?", title).Find(&posts)
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+	c.IndentedJSON(200, gin.H{
+		"post": posts,
 	})
 }
